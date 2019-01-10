bc86670954a11af50e4dc52dd11b596144ee6a39
clovesey <43855154+clovesey@users.noreply.github.com>
2019-01-10 15:21:55 +0000

    Add files via upload

--- root/js/main.js
+++ root/js/main.js
@@ -3,11 +3,12 @@ function choose(choice){
 item = 5;
 localStorage.setItem("key", item);
 }
+/*Loads the first slide automatically*/
 window.onload = function() {
 	item = 1;
 	showSlides(item)
 };
-
+/*switches the modes from nav bar to dropdown list*/
 function Dropdown() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -32,18 +33,24 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
+  /*Gets all of the items in the current html file with the class slide (the large images)*/
   var slides = document.getElementsByClassName("Slides");
-  var dots = document.getElementsByClassName("select");
+  /*Gets all of the items in the current html file with the class select (the small images)*/
+  var active = document.getElementsByClassName("select");
   var captionText = document.getElementById("caption");
+  /*Uses an array which is then looped through to check if its outside the bounds when the plus or minus 
+  arrow is used*/
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  /*Makes the current small image "active" so the opacity is set to 1*/
+  for (i = 0; i < active.length; i++) {
+    active[i].className = active[i].className.replace(" active", "");
   }
+  /*Makes the current large image "active" so the image is shown */
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  active[slideIndex-1].className += " active";
+  captionText.innerHTML = active[slideIndex-1].alt;
 }
